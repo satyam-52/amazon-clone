2940b3f677c2183979f81efe67a89743e869e9a0
Satyam Dua <69812664+satyam-52@users.noreply.github.com>
2021-04-15 22:52:27 +0530

    Update Order.js
    remove moments to avoid getting invalid date error at orders page..

--- src/Order.js
+++ src/Order.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Order.css";
-import moment from "moment";
+// import moment from "moment";  //Uncomment only after getting firebase blaze
 import CheckoutProduct from "./CheckoutProduct";
 import CurrencyFormat from "react-currency-format";
 
@@ -8,7 +8,8 @@ function Order({ order }) {
   return (
     <div className="order">
       <h2>Order</h2>
-      <p>{moment.unix(order.data.created).format("MMMM Do YYYY, h:mma")}</p>
+    {/*<p>{moment.unix(order.data.created).format("MMMM Do YYYY, h:mma")}</p>     //Uncomment only after getting firebase blaze and delete line 12..*/}
+      <p>{order.data.created}</p>
       <p className="order__id">
         <small>{order.id}</small>
       </p>
